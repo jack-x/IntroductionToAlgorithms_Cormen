8254a24738e3445bbd3db72696636968d7d4415c
Jack X <namanvrm7@gmail.com>
2017-03-23 13:45:34 +0530

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,26 @@
+#Selection Sort
+
+def main():
+	a = input().split()
+	arr =[]
+	for x in a:
+		arr.append(int(x))
+	
+	print("Original Array:{}".format( arr))
+	SelectionSort(arr)
+	print("Sorted Array:{}".format(arr))
+
+
+def SelectionSort(arr2):
+	for i in range(0,len(arr2)):
+		smallestIndex = i
+		for j in range(i,len(arr2)):
+			if arr2[smallestIndex]> arr2[j] :
+				smallestIndex=j
+		temp=arr2[i]
+		arr2[i]=arr2[smallestIndex]
+		arr2[smallestIndex]=temp
+	return arr2
+
+	
+if __name__ == "__main__": main()
