9df18989a7033c5a9b064e765e51408c7eb213a9
Jack X <namanvrm7@gmail.com>
2017-03-23 13:51:45 +0530

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -14,9 +14,13 @@ def main():
 def SelectionSort(arr2):
 	for i in range(0,len(arr2)):
 		smallestIndex = i
+		
+		#Searching for the ith Smallest element in the array
 		for j in range(i,len(arr2)):
 			if arr2[smallestIndex]> arr2[j] :
 				smallestIndex=j
+		
+		#Replacing the ith smallest element with the element at the ith position
 		temp=arr2[i]
 		arr2[i]=arr2[smallestIndex]
 		arr2[smallestIndex]=temp
